bf20a709629c3b89b9ea65454d21806a37888b26
Sudheerskb9 <sudheerskb9@gmail.com>
2024-07-31 11:56:27 +0530

    verifyToken added in APIs

--- src/app/api/teams/getQuery/route.js
+++ src/app/api/teams/getQuery/route.js
@@ -1,9 +1,7 @@
 import { getResponse } from "@/helpers/responseMessage";
 import { Query } from "@/modelCS/query";
 import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken'
-import { cookies } from "next/headers";
+import { verifyToken } from "@/helpers/tokenVerify";
 
 import { dbConnect } from "@/helpers/db"
 
@@ -11,27 +9,13 @@ export async function GET(request) {
 
   await dbConnect()
 
-  const cookie = cookies()
-
-  const tokenCookie= cookie.getAll()[0].value
-
-  console.log(tokenCookie)
-
   try{
 
-    if(!tokenCookie || tokenCookie == ''){
-        return NextResponse.json({
-            message: "Please login to access this page"
-        })
-    }
-  
+    const token = await verifyToken()
 
-  const token = jwt.verify(tokenCookie, process.env.SECRET_KEY)
-  console.log(token)
-
-
-    console.log(token.user.accessId)
-
+    if(token== "" || !token){
+      return NextResponse.json({message: "login required"});
+  }
     
     if(token && token.user.accessId ==1){
 
---
+++ src/app/api/teams/updateQuery/[queryId]/route.js
@@ -0,0 +1,51 @@
+import { getResponse } from "@/helpers/responseMessage";
+import { NextResponse } from "next/server";
+import { Query } from "@/modelCS/query";
+import { dbConnect } from "@/helpers/db";
+import { verifyToken } from "@/helpers/tokenVerify";
+
+
+export async function PUT(request,{params}) {
+
+  await dbConnect();
+
+  const {fullName, email, contact, businessName, queryContent, leadBy,assignTo,followUp,lastFollowUp,remarks} = await request.json()
+
+  try {
+
+    const token = await verifyToken()
+
+    if(token== "" || !token){
+        return NextResponse.json({message: "login required"});
+    }
+
+    const {queryId} = params
+
+    if (token && token.user.accessId == 1) {
+
+
+      const query = await Query.findById(queryId);
+      query.fullName = fullName,
+      query.email = email,
+      query.contact = contact,
+      query.businessName = businessName,
+      query.queryContent = queryContent,
+      query.leadBy = leadBy,
+      query.assignTo = assignTo,
+      query.followUp = followUp,
+      query.lastFollowUp = lastFollowUp,
+      query.remarks = remarks
+      
+      const updatedQuery = await query.save()
+
+      return NextResponse.json({
+        data: updatedQuery,
+        message: "Query Successfully Updated",
+        status: 200
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(error)
+  }
+}
