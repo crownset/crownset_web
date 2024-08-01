52ccffd4dfb23d0f7978f2b3b9ddf3d4a49948fb
Sudheerskb9 <sudheerskb9@gmail.com>
2024-08-01 18:47:18 +0530

    get Query validation

--- src/app/api/teams/addQuery/route.js
+++ src/app/api/teams/addQuery/route.js
@@ -10,27 +10,27 @@ export async function POST(request) {
 
   try {
 
-    if(fullName == ""){
+    if(!fullName || fullName == ""){
         return getResponse("Full Name is required", 500, false)
     }
 
-    else if(email == ""){
+    else if(!email || email == ""){
         return getResponse("Email is required field", 500, false)
     }
 
-    else if(contact == ""){
+    else if(!contact || contact == ""){
         return getResponse("Contact is required field", 500, false)
     }
 
-    else if(businessName == ""){
+    else if(!businessName || businessName == ""){
         return getResponse("Business Name is required field", 500, false)
     }
 
-    else if(queryContent == ""){
+    else if(!queryContent || queryContent == ""){
         return getResponse("Query Content is required field", 500, false)
     }
 
-    else if(leadBy == ""){
+    else if(!leadBy || leadBy == ""){
         return getResponse("Lead By is required field", 500, false)
     }
 
