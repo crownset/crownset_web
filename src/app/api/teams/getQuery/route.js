fb8006c544d7757d437af32de89c82496bd02a69
Sudheer <sudheerskb9@gmail.com>
2024-08-06 13:43:17 +0530

    populate users

--- src/app/api/teams/getQuery/route.js
+++ src/app/api/teams/getQuery/route.js
@@ -2,7 +2,7 @@ import { getResponse } from "@/helpers/responseMessage";
 import { Query } from "@/modelCS/query";
 import { NextResponse } from "next/server";
 import { verifyToken } from "@/helpers/tokenVerify";
-
+import userCS from '@/modelCS/user'
 import { dbConnect } from "@/helpers/db"
 
 export async function GET(request) {
@@ -18,7 +18,8 @@ export async function GET(request) {
   }
     if(token && token.user.accessId ==1){
 
-      const query = await Query.find({isDeleted:false}).sort({"queryDate":-1})
+      const query = await Query.find({isDeleted:false}).populate("assignTo","firstName",userCS).sort({"queryDate":-1})
+      console.log("this is populated", query)
       return NextResponse.json(query)
     }
 
