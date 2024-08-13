ecd01f33a279f4a8c62173ffc0efb3b88614a4dd
Sudheer <sudheerskb9@gmail.com>
2024-08-14 01:46:31 +0530

    UserCs fix

--- src/app/api/teams/getQuery/route.js
+++ src/app/api/teams/getQuery/route.js
@@ -2,7 +2,7 @@ import { getResponse } from "@/helpers/responseMessage";
 import { Query } from "@/modelCS/query";
 import { NextResponse } from "next/server";
 import { verifyToken } from "@/helpers/tokenVerify";
-import UserCS from '@/modelCS/user'
+import {UserCS} from '@/modelCS/user'
 import { dbConnect } from "@/helpers/db"
 
 export async function GET(request) {
