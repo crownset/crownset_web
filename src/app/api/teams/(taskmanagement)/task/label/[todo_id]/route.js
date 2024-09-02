da27c50df8cb9cf57e04215ed7f12a07a6ad02a2
shadab <shadabnid@gmail.com>
2024-09-02 15:30:46 +0530

    fixed issues in task

--- src/app/api/teams/(taskmanagement)/task/label/[todo_id]/route.js
+++ src/app/api/teams/(taskmanagement)/task/label/[todo_id]/route.js
@@ -1,20 +1,32 @@
 import Todo from "@/modelCS/todo";
 import { dbConnect } from '@/helpers/db'
 import { NextResponse } from "next/server";
+import jwt from 'jsonwebtoken'
 
 export async function PUT(req, { params }) {
     await dbConnect();
 
-    const { todo_id } = params;
-    const { selectedLabel } = await req.json();
 
 
     try {
+        // console.log("start")
+        const { todo_id } = params;
+        const { selectedLabel } = await req.json();
 
+        const token = req.cookies.get("authToken:")?.value || '';
 
+        if (!token) {
+            return NextResponse.json({ message: "Please Login First" }, { status: 401 })
+        }
+
+        const decode = await jwt.verify(token, process.env.SECRET_KEY)
+        if (!decode) {
+            return NextResponse.json({ message: "You are not authorized" }, { status: 401 })
+        }
+        
         const updatedTodo = await Todo.findByIdAndUpdate(
             { _id: todo_id },
-            { label: selectedLabel },
+            { $set: { label: selectedLabel } },
             { new: true }
         );
 
