71d75b6a6784e8978d8c07988c2da0527533a211
Sudheer <sudheerskb9@gmail.com>
2024-09-03 12:02:39 +0530

    validation in import functionallity and add token expiration time of 1 hr in login and reset functionallity

--- src/app/api/teams/addMultipleQueries/route.js
+++ src/app/api/teams/addMultipleQueries/route.js
@@ -2,55 +2,20 @@ import { getResponse } from "@/helpers/responseMessage";
 import { NextResponse } from "next/server";
 import { Query } from "@/modelCS/query";
 import { dbConnect } from "@/helpers/db"
-// import nodeMailer from 'nodemailer';
-// import { verifyToken } from "@/helpers/tokenVerify";
-
-// const userName = process.env.EMAIL_ID;
-// const passWord = process.env.EMAIL_PASS;
-
-// const transporter = nodeMailer.createTransport({
-//   service: 'gmail',
-//   host: 'smtp.gmail.com',
-//   secure: false,
-//   auth: {
-//       user: userName,
-//       pass: passWord
-//   }
-// })
-
-// async function mailResponse(fullName,email){
- 
-//   const mailOptions = {
-//      from: 'thecrownset@gmail.com',
-//      to: `${email}`,
-//      subject: 'check mail',
-//      html: `Dear ${fullName},
-//      <br>
-//      <br>
-//      Thank you for reaching out to Crownset. We appreciate your interest and value your inquiry.
-//      <br>
-//      <br>
-//      One of our executives will be in touch with you shortly to discuss your needs and provide further assistance. If you have any immediate questions or need further information, please don’t hesitate to let us know.
-//      <br><br>
-//      Thank you once again for contacting us. We look forward to speaking with you soon.
-//      <br>
-//      <br>
-//      Best regards,
-//      <br>
-//      Crownset Marketing Agency
-//      <br>
-//      +91 8168695799`
-//  }
-
-//   return transporter.sendMail(mailOptions)
-
-// }
+import { verifyToken } from "@/helpers/tokenVerify";
 
 export async function POST(request) {
   await dbConnect()
   const { queryData } = await request.json();
 
   try {
+
+    const token = await verifyToken()
+
+    if(token== "" || !token){
+      return NextResponse.json({message: "login required"});
+  }
+    if((token && token.user.accessId ==1) || (token && token.user.accessId ==2)){
 
     for (const query of queryData) {
 
@@ -78,12 +43,19 @@ export async function POST(request) {
     return getResponse("Comments is required field", 500, false)
 }
     }
+  
     const savedQueries = await Query.insertMany(queryData);
 
     return NextResponse.json({
       data: savedQueries,
       message: "Query Saved Sucessfully",
     });
+  }
+
+  else{
+    return getResponse("You Dont have a right credentials", 500, false)
+}
+
   } catch (error) {
     return getResponse("error in posting query", 500, false);
   }
