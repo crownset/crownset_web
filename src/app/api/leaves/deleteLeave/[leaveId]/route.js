4ea289e2e37983d81b4d228d3b43f1bb2ce88753
Sudheer <sudheerskb9@gmail.com>
2024-09-05 15:48:12 +0530

    update the code for add leave and update the leave when user delete before start date

--- src/app/api/leaves/deleteLeave/[leaveId]/route.js
+++ src/app/api/leaves/deleteLeave/[leaveId]/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { Leave } from "@/modelCS/leave";
 import { dbConnect } from "@/helpers/db";
 import { verifyToken } from "@/helpers/tokenVerify";
+import { UserCS } from "@/modelCS/user";
 
 
 export async function PUT(request, { params }) {
@@ -30,12 +31,39 @@ export async function PUT(request, { params }) {
                 return NextResponse.json({message: "You can't delete"})
             }
               }
-
+              
             leave.isDeleted = true
+            const updatedLeave = await leave.save()
+
+            const user = await UserCS.findById(leave.userId)
+
+            if(leave.leaveType == 'Full Day'){
+
+                const eDate = new Date(leave.endDate);
+                const sDate = new Date(updatedLeave.startDate);
+      
+                eDate.setHours(0, 0, 0, 0)
+                sDate.setHours(0, 0, 0, 0)
+      
+                const takenLeave = ((eDate-sDate)/(1000 * 60 * 60 * 24))+1
+      
+                user.leaveBalance += takenLeave
+      
+                const updatedUser = await user.save()
+              }
+      
+              if(updatedLeave.leaveType == 'Half Day'){
+                
+                const takenLeave = 0.5
+      
+                user.leaveBalance += takenLeave
+      
+                const updatedUser = await user.save()
+              }
 
             // logic for if we delete the leave after approved before the start date the leave added
 
-            const updatedLeave = await leave.save()
+
 
             return NextResponse.json({
                 data: updatedLeave,
--- src/app/api/leaves/updateLeave/[leaveId]/route.js
+++ src/app/api/leaves/updateLeave/[leaveId]/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { Leave } from "@/modelCS/leave";
 import { dbConnect } from "@/helpers/db";
 import { verifyToken } from "@/helpers/tokenVerify";
-
+import { UserCS } from "@/modelCS/user";
 
 export async function PUT(request, { params }) {
 
@@ -40,8 +40,37 @@ export async function PUT(request, { params }) {
 
       leave.status = status
 
-
       const updatedLeave = await leave.save()
+
+      const user = await UserCS.findById(updatedLeave.userId)
+
+      if(updatedLeave.status == 'Approved'){
+        if(updatedLeave.leaveType == 'Full Day'){
+
+          const eDate = new Date(updatedLeave.endDate);
+          const sDate = new Date(updatedLeave.startDate);
+
+          eDate.setHours(0, 0, 0, 0)
+          sDate.setHours(0, 0, 0, 0)
+
+          const takenLeave = ((eDate-sDate)/(1000 * 60 * 60 * 24))+1
+
+          user.leaveBalance -= takenLeave
+
+          const updatedUser = await user.save()
+        }
+
+        if(updatedLeave.leaveType == 'Half Day'){
+
+          
+          const takenLeave = 0.5
+
+          user.leaveBalance -= takenLeave
+
+          const updatedUser = await user.save()
+        }
+        console.log("updatedLeave>>>>",updatedLeave)
+      }
 
       return NextResponse.json({
         data: updatedLeave,
