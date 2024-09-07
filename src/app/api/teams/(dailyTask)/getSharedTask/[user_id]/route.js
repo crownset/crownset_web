88c8e67a97f84ba6dfd51eb160fc3fc7b5231efc
shadab <shadabnid@gmail.com>
2024-09-07 14:36:47 +0530

    added shared route

---
+++ src/app/api/teams/(dailyTask)/getSharedTask/[user_id]/route.js
@@ -0,0 +1,37 @@
+import { dbConnect } from "@/helpers/db";
+import { NextResponse } from "next/server";
+import DailyTask from "@/modelCS/dailyTask";
+import jwt from 'jsonwebtoken'
+
+export async function GET(req, { params }) {
+    try {
+        await dbConnect();
+        const { user_id } = params;
+        
+       console.log(user_id);
+        const token = req.cookies.get("authToken:")?.value || '';
+        if (!token) {
+            return NextResponse.json({ message: "Please Login First" }, { status: 401 });
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+        if (!decoded) {
+            return NextResponse.json({ message: "You are not authorized" }, { status: 401 });
+        }
+
+        const share_with = decoded.user._id
+        console.log(share_with);
+        const tasks = await DailyTask.find({ _id:user_id,share_with:share_with }).sort({ createdAt: -1 })
+        console.log(tasks);
+
+        if (tasks.length === 0) {
+            return NextResponse.json({ message: "No Task Found", tasks: [] }, { status: 400 })
+        }
+
+        return NextResponse.json({ message: "Tasks fetched successfully"}, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ message: 'Error fetching tasks', error: error.message }, { status: 500 });
+    }
+}
--- src/app/api/teams/(dailyTask)/share/[task_id]/route.js
+++ src/app/api/teams/(dailyTask)/share/[task_id]/route.js
@@ -3,47 +3,42 @@ import DailyTask from '@/modelCS/dailyTask.js';
 import jwt from 'jsonwebtoken';
 import { NextResponse } from "next/server";
 
-export async function POST(req, { params }) {
-    await dbConnect();
+export async function PUT(req, { params }) {
 
-    const { task_id } = params;
 
     try {
-        console.count("here");
-        const { user_id } = await req.json();
+        await dbConnect();
+        const { task_id } = params;
 
-        console.count("Here");
+        const { share_with_id } = await req.json();
+        console.log("here");
+
         const token = req.cookies.get("authToken:")?.value || '';
         if (!token) {
-            return NextResponse.json({ message: "Please Login First" }, { status: 401 })
+            return NextResponse.json({ message: "Please Login First" }, { status: 401 });
         }
 
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
         if (!decoded) {
-            return NextResponse.json({ message: "You are not authorized" }, { status: 401 })
+            return NextResponse.json({ message: "You are not authorized" }, { status: 401 });
         }
 
+        let task = await DailyTask.findById(task_id);
 
-        let task = await DailyTask.findById({ _id: task_id });
-        console.count("Here");
         if (!task) {
             return NextResponse.json({ message: 'Task not found' }, { status: 404 });
         }
 
-        // Add users to share_with array, avoiding duplicates
 
-        console.count("Here");
-       
-        if (!task.share_with.includes(user_id)) {
-            task = await DailyTask.findOneAndUpdate(
-                { _id: task_id },
-                { $push: { share_with: user_id } },
+        if (!task.share_with.includes(share_with_id)) {
+
+            task = await DailyTask.findByIdAndUpdate(
+                task_id,
+                { $push: { share_with: share_with_id } },
                 { new: true }
             );
         }
-        console.count("Here");
-        
 
         return NextResponse.json({ message: 'Task shared successfully', task }, { status: 200 });
     } catch (error) {
