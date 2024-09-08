2eea6f1ac088c34a5afec93bb6b1a67ba0c06e84
shadab <shadabnid@gmail.com>
2024-09-08 11:35:02 +0530

    dailytask complete

--- src/app/api/teams/(dailyTask)/getSharedTask/[user_id]/route.js
+++ src/app/api/teams/(dailyTask)/getSharedTask/[user_id]/route.js
@@ -20,16 +20,16 @@ export async function GET(req, { params }) {
             return NextResponse.json({ message: "You are not authorized" }, { status: 401 });
         }
 
-        const share_with = decoded.user._id
-        console.log(share_with);
-        const tasks = await DailyTask.find({ _id:user_id,share_with:share_with }).sort({ createdAt: -1 })
-        console.log(tasks);
+        const share_with_id = decoded.user._id
+        // console.log(share_with);
+        const tasks = await DailyTask.find({ created_by:user_id,share_with:share_with_id }).sort({ createdAt: -1 })
+        // console.log(tasks);
 
         if (tasks.length === 0) {
             return NextResponse.json({ message: "No Task Found", tasks: [] }, { status: 400 })
         }
 
-        return NextResponse.json({ message: "Tasks fetched successfully"}, { status: 200 });
+        return NextResponse.json({ message: "Tasks fetched successfully",tasks}, { status: 200 });
 
     } catch (error) {
         return NextResponse.json({ message: 'Error fetching tasks', error: error.message }, { status: 500 });
