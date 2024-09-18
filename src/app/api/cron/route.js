5446dc0d7f51e35f6f7f9b1237d2f2e97b364c70
Sudheer <sudheerskb9@gmail.com>
2024-09-18 11:45:52 +0530

    comment cron code

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,39 +1,39 @@
 
-import { UserCS } from "@/modelCS/user";
-import { NextResponse } from "next/server";
-import { dbConnect } from "@/helpers/db";
+// import { UserCS } from "@/modelCS/user";
+// import { NextResponse } from "next/server";
+// import { dbConnect } from "@/helpers/db";
 
-export async function GET(request) {
+// export async function GET(request) {
 
-    await dbConnect()
+//     await dbConnect()
 
-    try{
+//     try{
 
-        console.log('Cron job running at 18.45');
+//         console.log('Cron job running at 18.45');
 
-        const users = await UserCS.find({});
+//         const users = await UserCS.find({});
     
-        users.forEach(async (user) => {
-            const newLeaveBalance = Math.min(user.leaveBalance + 1.25, 15); // Cap at 18
-            user.leaveBalance = newLeaveBalance;
-            await user.save();
-        });
+//         users.forEach(async (user) => {
+//             const newLeaveBalance = Math.min(user.leaveBalance + 1.25, 15); // Cap at 18
+//             user.leaveBalance = newLeaveBalance;
+//             await user.save();
+//         });
 
-        return NextResponse.json({
-            message: "cron job works"
-        })
+//         return NextResponse.json({
+//             message: "cron job works"
+//         })
 
-    }
+//     }
 
-    catch(error){
+//     catch(error){
 
-        console.log(error)
+//         console.log(error)
 
-        return NextResponse.json({
-            message: "error in post request",
-            status: 300
-        })
+//         return NextResponse.json({
+//             message: "error in post request",
+//             status: 300
+//         })
 
-    }
+//     }
 
-}
+// }
