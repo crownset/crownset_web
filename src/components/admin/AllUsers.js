484a7725360645de30312dcbcca9034796027369
shadab <shadabnid@gmail.com>
2024-09-27 12:23:55 +0530

    add attendance table

--- src/components/admin/AllUsers.js
+++ src/components/admin/AllUsers.js
@@ -1,6 +1,6 @@
 "use client";
 import { useDispatch, useSelector } from 'react-redux';
-import {useState, useEffect } from 'react';
+import {useState, useEffect, useCallback } from 'react';
 import { assignUsers } from '@/redux/slices/userSlice';
 import { CustomLoader } from '@/components/CustomLoader';
 import { useRouter } from 'next/navigation';
@@ -10,13 +10,24 @@ const AllUsers = () => {
   const dispatch = useDispatch();
     
     const { user, loading } = useSelector((state) => state.user); 
+    const [filteredUser,setFilteredUser] = useState([]);
+    const [originalUser,setOriginalUser] = useState([]);
+
     const router = useRouter();
 
-  
-
+    
     useEffect(() => {
         dispatch(assignUsers());
+
+        
     }, [dispatch]);
+
+    useEffect(() => {
+        if (user?.length > 0) {
+          setOriginalUser(user);
+          setFilteredUser(user.filter((u) => u.accessId === 2)); // Filtering users with accessId === 2
+        }
+      }, [user]); 
 
   
     const handleAttendanceView = (id) => {
@@ -35,14 +46,14 @@ const AllUsers = () => {
                     </div>
 
                     <div className="w-full flex flex-wrap justify-center gap-4 items-start">
-                        {user?.length > 0 ? (
-                            user.map((user) => (
+                        {filteredUser?.length > 0 ? (
+                            filteredUser.map((user) => (
                                 <div
                                     key={user._id}
                                     className="flex cursor-pointer flex-col justify-center items-center bg-dashboardUserBg px-4 py-3 rounded-2xl text-center shadow-md"
                                     onClick={() => handleAttendanceView(user?._id)} 
                                 >
-                                    <div className="text-lg font-medium">{user.firstName}</div>
+                                    <div className="text-lg font-medium">{user?.firstName}</div>
                                     <div className="text-sm font-light text-gray-500">View Attendance</div>
                                 </div>
                             ))
